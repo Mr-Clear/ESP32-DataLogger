85ea4a3cd51c056634014cd0d123c64124bf6cdd
Thomas <git@thomas.klierlinge.de>
2023-03-25 08:07:14 +0100

    Push data to www.klierlinge.de

--- test.ino
+++ test.ino
@@ -14,6 +14,7 @@ https://github.com/Risele/SHT3x
 #include <esp_adc_cal.h>
 #include <esp_log.h>
 #include <esp_sntp.h>
+#include <HTTPClient.h>
 #include <OneWire.h>
 #include <SHT3x.h>
 #include <WiFi.h>
@@ -26,7 +27,7 @@ https://github.com/Risele/SHT3x
 #define BUTTON_2 35
 
 TFT tft;
-SHT3x Sensor;
+SHT3x Sht30Sensor;
 int vref = 1100;
 
 OneWire oneWire(ONE_WIRE_BUS);
@@ -57,8 +58,8 @@ void setup(void) {
       Serial.println("Default Vref: 1100mV");
   }
 
-  tft.drawString("Init Temp/Hum Sensor...   ", 0, 0);
-  Sensor.Begin();
+  tft.drawString("Init SHT30 Sensor...   ", 0, 0);
+  Sht30Sensor.Begin();
 
   tft.drawString("Init Temp Sensors...      ", 0, 0);
   sensors.begin();
@@ -115,8 +116,8 @@ void loop() {
   //String time = String(esp_log_timestamp() / 1000);
 
   uint16_t v = analogRead(ADC_PIN);
-  float battery_voltage = ((float)v / 4095.0) * 2.0 * 3.3 * (vref / 1000.0);
-  String voltage = "VCC: " + String(battery_voltage) + " V  ";
+  double voltage = v / 4095.0 * 2.0 * 3.3 * (vref / 1000.0);
+  String voltageString = "VCC: " + String(voltage) + " V  ";
 
   wl_status_t wifiStatus = WiFi.status();
   String wifiStatusString = "WIFI:" + wifiStatusToString(wifiStatus) + "             ";
@@ -124,31 +125,37 @@ void loop() {
   }
   lastWifiStatus = wifiStatus;
 
-  Sensor.UpdateData();
-  uint8_t sensorError = Sensor.GetError();
-  String temperature;
-  String humidity;
+  Sht30Sensor.UpdateData();
+  uint8_t sensorError = Sht30Sensor.GetError();
+  double sht30Temperature = NAN;
+  double sht30Humidity = NAN;
+  String sht30TemperatureString;
+  String sht30HumidityString;
   if (sensorError) {
-    temperature = "Tmp: Error " + String(sensorError) + "    ";
-    humidity = "Hum: Error " + String(sensorError) + "    ";
+    sht30TemperatureString = "Tmp: Error " + String(sensorError) + "    ";
+    sht30HumidityString = "Hum: Error " + String(sensorError) + "    ";
   } else {
-    temperature = "Tmp: " + String(Sensor.GetTemperature()) + " °C  ";
-    humidity = "Hum: " + String(Sensor.GetRelHumidity()) + " %  ";
+    sht30Temperature = Sht30Sensor.GetTemperature();
+    sht30Humidity = Sht30Sensor.GetRelHumidity();
+    sht30TemperatureString = "Tmp: " + String(sht30Temperature) + " °C  ";
+    sht30HumidityString = "Hum: " + String(sht30Humidity) + " %  ";
   }
 
-  std::vector<String> tempSensors;
+  std::vector<double> sht30Values;
+  std::vector<String> sht30Strings;
   sensors.requestTemperatures();
   for (int i=0; i < numberOfDevices; i++) {
     if (sensors.getAddress(tempDeviceAddress, i)) {
       float tempC = sensors.getTempC(tempDeviceAddress);
-      tempSensors.emplace_back("Tm:" + String(i) + " " + String(tempC) + " °C");
+      sht30Values.emplace_back(tempC);
+      sht30Strings.emplace_back("Tm:" + String(i) + " " + String(tempC) + " °C");
     }
   }
 
   String button1 = "BT1: " + String(digitalRead(BUTTON_1) ? "Up     " : "Down ");
   String button2 = "BT2: " + String(digitalRead(BUTTON_2) ? "Up     " : "Down ");
 
-  String ip = WiFi.localIP().toString();
+  String ip = WiFi.localIP().toString() + "   ";
 
   tft.setRotation(3);
   tft.setTextColor(Color::White, Color::Black);
@@ -163,23 +170,27 @@ void loop() {
   tft.drawString(fps, x, y+=dy);
   tft.drawString(interval, x, y+=dy);
   tft.drawString(lastDurationString, x, y+=dy);
-  tft.drawString(voltage, x, y+=dy);
-  tft.drawString(temperature, x, y+=dy);
-  tft.drawString(humidity, x, y+=dy);
+  tft.drawString(voltageString, x, y+=dy);
+  tft.drawString(sht30TemperatureString, x, y+=dy);
+  tft.drawString(sht30HumidityString, x, y+=dy);
   tft.drawString(wifiStatusString, x, y+=dy);
 
   y = -dy;
   tft.drawString("", x, y+=dy);
   x = tft.size.y() / 2;
-  for (const String &s : tempSensors) {
+  for (const String &s : sht30Strings) {
     tft.drawString(s, x, y+=dy);
   }
   tft.drawString(button1, x, y+=dy);
   tft.drawString(button2, x, y+=dy);
   tft.drawString(ip, x, y+=dy);
 
-  const int desiredInterval = 1000;
+  if (wifiStatus == WL_CONNECTED) {
+    int httpResponseCode = sendData(lastDuration, voltage, sht30Temperature, sht30Humidity, sht30Values);
+    tft.drawString(ip, tft.size.y() -50, y);
+  }
 
+  const int desiredInterval = 1000;
   lastDuration = millis() - start;
   lastDelay = constrain(desiredInterval - lastDuration, 0, desiredInterval);
   delay(lastDelay);
@@ -208,6 +219,7 @@ String wifiStatusToString(wl_status_t status) {
 }
 
 String getTime() {
+  return String(millis() / 1000);
   struct tm timeinfo;
   if (!getLocalTime(&timeinfo)) {
     return String(millis() / 1000);
@@ -216,4 +228,22 @@ String getTime() {
   char buf[64];
   size_t written = strftime(buf, 64, "%a, %Y-%m-%d %H:%M:%S", &timeinfo);
   return String(buf, written);
+}
+
+int sendData(unsigned long duration, double voltage, double sht30Temp, double sht30Hum, const std::vector<double> &ds18b20)
+{
+  Serial.print("HTTPClient begin... ");
+  HTTPClient http;
+  http.begin("https://www.klierlinge.de/rrd/update");
+  String data = String("{\"args\": [\"esp32test.rrd\", \"N") +
+                ":" + String(duration) +
+                ":" + String(voltage) +
+                ":" + (!std::isnan(sht30Temp) ? String(sht30Temp) : "U") +
+                ":" + (!std::isnan(sht30Hum) ? String(sht30Hum) : "U") +
+                ":" + (ds18b20.size() > 0 ? String(ds18b20[0]) : "U") +
+                ":" + (ds18b20.size() > 1 ? String(ds18b20[1]) : "U") +
+                ":" + (ds18b20.size() > 2 ? String(ds18b20[2]) : "U") + "\"]}";
+  int httpResponseCode = http.POST(data);
+  http.end();
+  return httpResponseCode;
 }
