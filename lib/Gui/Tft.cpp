2b8004415605e82f677edeedd40b26d9fb7cdbd3
Thomas <git@thomas.klierlinge.de>
2023-04-08 12:06:34 +0200

    Tft class doesn't derive from TFT_eSPI

--- lib/Gui/Tft.cpp
+++ lib/Gui/Tft.cpp
@@ -1,42 +1,61 @@
 #include "Tft.h"
 
-const Vector2i &Tft::size()
-{
-  if (getRotation() % 2)
+#include <SPI.h>
+#include <TFT_eSPI.h>
+
+Tft::Tft() :
+  _tft(new TFT_eSPI{}) { }
+
+Tft::~Tft() = default;
+
+const Vector2i &Tft::size() {
+  if (_tft->getRotation() % 2)
     return _sizeR;
   return _size;
 }
 
-void Tft::drawRect(const Vector2i &topLeft, const Vector2i &size, Color color, bool filled)
-{
+void Tft::init() {
+  _tft->init();
+}
+
+void Tft::setRotation(uint8_t rotation) {
+  _tft->setRotation(rotation);
+}
+
+void Tft::fillScreen(const Color &color) {
+  _tft->fillScreen(color);
+}
+
+void Tft::drawRect(const Vector2i &topLeft, const Vector2i &size, Color color, bool filled) {
   if (filled)
-  {
-    TFT_eSPI::fillRect(topLeft.x(), topLeft.y(), size.x(), size.y(), color);
-  }
+    _tft->fillRect(topLeft.x(), topLeft.y(), size.x(), size.y(), color);
   else
-  {
-    TFT_eSPI::drawRect(topLeft.x(), topLeft.y(), size.x(), size.y(), color);
-  }  
+    _tft->drawRect(topLeft.x(), topLeft.y(), size.x(), size.y(), color);
 }
 
-void Tft::drawEllipse(const Vector2i &center, const Vector2i &size, Color color, bool filled)
-{
+void Tft::drawEllipse(const Vector2i &center, const Vector2i &size, Color color, bool filled) {
   if (filled)
-  {
-    TFT_eSPI::fillEllipse(center.x(), center.y(), size.x(), size.y(), color);
-  }
+    _tft->fillEllipse(center.x(), center.y(), size.x(), size.y(), color);
   else
-  {
-    TFT_eSPI::drawEllipse(center.x(), center.y(), size.x(), size.y(), color);
-  }
+    _tft->drawEllipse(center.x(), center.y(), size.x(), size.y(), color);
 }
 
-void Tft::drawString(const char *string, const Vector2i &position)
-{
-  TFT_eSPI::drawString(string, position.x(), position.y());
+void Tft::loadFont(const uint8_t font[]) {
+  _tft->loadFont(font);
 }
 
-void Tft::drawString(const String &string, const Vector2i &position)
-{
-  TFT_eSPI::drawString(string, position.x(), position.y());
+void Tft::setTextColor(const Color &forground) {
+  _tft->setTextColor(forground);
+}
+
+void Tft::setTextColor(const Color &forground, const Color &background) {
+  _tft->setTextColor(forground, background);
+}
+
+void Tft::drawString(const char *string, const Vector2i &position) {
+  _tft->drawString(string, position.x(), position.y());
+}
+
+void Tft::drawString(const String &string, const Vector2i &position) {
+  _tft->drawString(string, position.x(), position.y());
 }
--- lib/Gui/Tft.h
+++ lib/Gui/Tft.h
@@ -1,26 +1,39 @@
 #pragma once
 
-#include <sys/_stdint.h>
-
 #include "Color.h"
 #include "Vector.h"
 
-#include <SPI.h>
-#include <TFT_eSPI.h>
+#include <WString.h>
+
+#include <memory>
+#include <sys/_stdint.h>
+
+class TFT_eSPI;
 
 using Vector2i = Vector2<int>;
 
-class Tft : public TFT_eSPI
+class Tft
 {
 public:
+  Tft();
+  ~Tft();
+
   const Vector2i &size();
 
+  void init();
+  void setRotation(uint8_t rotation);
+  void fillScreen(const Color &color);
   void drawRect(const Vector2i &topLeft, const Vector2i &size, Color color, bool filled = false);
   void drawEllipse(const Vector2i &center, const Vector2i &size, Color color, bool filled = false);
+  void loadFont(const uint8_t font[]);
+  void setTextColor(const Color &forground);
+  void setTextColor(const Color &forground, const Color &background);
   void drawString(const char *string, const Vector2i &position);
   void drawString(const String &string, const Vector2i &position);
 
 private:
+  std::unique_ptr<TFT_eSPI> _tft;
+
   const Vector2i _size{135, 240};
   const Vector2i _sizeR{~_size};
 };
