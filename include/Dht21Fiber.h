1e0c2c91718cd7947e0e4034d773b8f812da23e0
Thomas <git@thomas.klierlinge.de>
2024-10-09 09:11:08 +0200

    Add fiber for DHT21 sensor

---
+++ include/Dht21Fiber.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <Fiber.h>
+#include <Observable.h>
+#include <WString.h>
+
+#include <memory>
+#include <map>
+
+class DHT_Unified;
+
+class Dht21Fiber : public Fiber {
+public:
+  struct Data {
+    float temperature;
+    float humidity;
+    bool operator==(const Data&) const = default;
+  };
+
+  Dht21Fiber(uint8_t pin);
+  ~Dht21Fiber();
+
+  const Observable<Data> &data();
+  void scan();
+  
+protected:
+  void setup() override;
+  void loop() override;
+
+private:
+  void initSensor();
+
+  std::unique_ptr<DHT_Unified> _sensor;
+  ObservableValue<Data> _data;
+  uint8_t _pin;
+  ulong _minDelay = 0;
+  bool _rescan = false;
+};
---
+++ src/Dht21Fiber.cpp
@@ -0,0 +1,47 @@
+#include "Dht21Fiber.h"
+
+#include <DHT_U.h>
+
+Dht21Fiber::Dht21Fiber(uint8_t pin) :
+  _pin{pin}
+{ }
+
+Dht21Fiber::~Dht21Fiber() = default;
+
+const Observable<Dht21Fiber::Data> &Dht21Fiber::data() {
+    return _data;
+}
+
+void Dht21Fiber::scan() {
+    _rescan = true;
+}
+
+void Dht21Fiber::setup() {
+    initSensor();
+}
+
+void Dht21Fiber::loop() {
+    if (_rescan) {
+        initSensor();
+        _rescan = false;
+    }
+    static ulong lastUpdate = 0;
+    const ulong now = micros();
+    if (now - lastUpdate < _minDelay)
+        return;
+    lastUpdate = now;
+    Data data;
+    sensors_event_t event;
+    _sensor->temperature().getEvent(&event);
+    data.temperature = event.temperature;
+    _sensor->humidity().getEvent(&event);
+    data.humidity = event.relative_humidity;
+    _data = data;
+}
+
+void Dht21Fiber::initSensor() {
+    _sensor.reset(new DHT_Unified(_pin, DHT21));
+    sensor_t sensor;
+    _sensor->temperature().getSensor(&sensor);
+    _minDelay = sensor.min_delay;
+}
